983c333b095ca006081cc0099c5b0e007a6bf838
pegahposhtareh <pegah.poshtareh@hhu.de>
2015-11-17 18:35:18 +0100

    mean and variance is calculated

--- statistics.cxx
+++ statistics.cxx
@@ -1,18 +1,38 @@
 #include <cmath>
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+
+
+void Random (double* x , const int N){
+  for(int i = 0 ; i < N ; i++){
+  x[i] = ((double)rand() / (RAND_MAX));
+  }
+}
+
+void mean_variance (double* x ,const int N , double& m , double& v ){
+  m = 0.0;
+  v = 0.0;
+
+for( int i = 0 ; i < N-1 ; i++){
+    m += 1.0/N * (x[i]);
+    cout << x[i] << endl;
+    }
+for( int i = 0 ; i < N-1 ; i++){
+    v += 1.0/N * pow((x[i] - m),2);
+    }
+}
 
 int main(){
    const int N = 100;
    double p[N];
    double mean, var;
 
-   // Some lines here....
+   srand(time(NULL));
+   Random (p , N);
+   mean_variance (p, N, mean , var);
 
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
